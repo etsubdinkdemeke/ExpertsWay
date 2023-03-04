f61189eec993b6d9932e9097496e2088183fc3bf
Yoko hailemariam <yokohailemariam@gmai.com>
2023-03-04 16:53:44 +0300

    renamed whihteBtn to isPicked

--- lib/models/programming_options_model.dart
+++ lib/models/programming_options_model.dart
@@ -1,11 +1,11 @@
 class ProgrammingOptionsModel {
   final String btnName;
-  bool whiteBtn;
+  bool isPicked;
   String iconUrl;
 
   ProgrammingOptionsModel({
     required this.btnName,
-    required this.whiteBtn,
+    required this.isPicked,
     this.iconUrl = 'https://cdn-icons-png.flaticon.com/512/6062/6062646.png',
   });
 }
@@ -13,35 +13,35 @@ class ProgrammingOptionsModel {
 List<ProgrammingOptionsModel> ProgrammingOptionsModels = [
   ProgrammingOptionsModel(
       btnName: 'JavaScript',
-      whiteBtn: true,
+      isPicked: true,
       iconUrl: 'https://cdn-icons-png.flaticon.com/512/5968/5968292.png'),
   ProgrammingOptionsModel(
       btnName: 'Python',
-      whiteBtn: true,
+      isPicked: true,
       iconUrl: 'https://cdn-icons-png.flaticon.com/512/5968/5968350.png'),
   ProgrammingOptionsModel(
       btnName: 'Java',
-      whiteBtn: true,
+      isPicked: true,
       iconUrl: 'https://cdn-icons-png.flaticon.com/512/5968/5968282.png'),
   ProgrammingOptionsModel(
       btnName: 'Go',
-      whiteBtn: true,
+      isPicked: true,
       iconUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Go_Logo_Blue.svg/1200px-Go_Logo_Blue.svg.png'),
   ProgrammingOptionsModel(
       btnName: 'C++',
-      whiteBtn: true,
+      isPicked: true,
       iconUrl: 'https://cdn-icons-png.flaticon.com/512/6132/6132222.png'),
   ProgrammingOptionsModel(
       btnName: 'PHP',
-      whiteBtn: true,
+      isPicked: true,
       iconUrl: 'https://cdn-icons-png.flaticon.com/512/5968/5968332.png'),
   ProgrammingOptionsModel(
       btnName: 'SQL',
-      whiteBtn: true,
+      isPicked: true,
       iconUrl: 'https://cdn-icons-png.flaticon.com/512/919/919836.png'),
   ProgrammingOptionsModel(
       btnName: 'Ruby',
-      whiteBtn: true,
+      isPicked: true,
       iconUrl: 'https://cdn-icons-png.flaticon.com/512/919/919842.png'),
 ];
