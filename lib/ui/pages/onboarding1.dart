edd4950180e7ef6f859c404d5256e924198c3ab5
sami-bre <samuelbirhanu121@gmail.com>
2023-03-07 23:41:22 +0300

    reinnovated the onboarding page to make it resemble the newly updated design. now the 3 states of the page do a fade-transition (as depicted on the design)

--- lib/ui/pages/onboarding1.dart
+++ lib/ui/pages/onboarding1.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:learncoding/api/google_signin_api.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Onboarding extends StatefulWidget {
   @override
@@ -14,114 +15,176 @@ class Onboarding extends StatefulWidget {
 class _OnboardingState extends State<Onboarding> {
   final PageController controller = PageController(initialPage: 0);
 
-  int? pageNumber;
-  List widgets = [];
+  int counter = 0;
+  List<Widget> pictures = [];
+  List<Widget> messages = [];
+  List<Widget> buttons = [];
+  late Widget displayedPicture;
+  late Widget displayedMessage;
+  late Widget displayedButton;
+
   @override
   void initState() {
-    pageNumber = 0;
     super.initState();
     // signin();
   }
 
   void createWidgets() {
-    widgets.addAll([
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image.asset('assets/images/1.png'),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.6,
-            child: Text(
-              "Easy access to video lectures, & reading materials.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 14,
-                  color: Color(0xFFFFFFFF)),
-            ),
-          )
-        ],
-      ),
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image.asset('assets/images/2.png'),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.6,
-            child: Text(
-              "Ask questions, earn coins and dominate the global leaderboard.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 14,
-                  color: Color(0xFFFFFFFF)),
-            ),
-          )
-        ],
-      ),
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image.asset('assets/images/logo.png'),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.6,
-            child: Text(
-              "E-Learn",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 28,
-                  color: Color(0xFFFFFFFF)),
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.3,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.6,
-            child: Text(
-              "The complete E-learning solution for students of all ages!\n\n\nJoin for FREE now!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 14,
-                  color: Color(0xFFFFFFFF)),
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.05,
-          ),
-          CupertinoButton(
-              color: Color(0xFFFFFFFF),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    "Sign in with Google ➡",
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: Color(0xFF0083BE),
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              onPressed: signin)
-        ],
-      ),
+    pictures.addAll([
+      buildSvgPicture('assets/images/freelancer.svg'),
+      buildSvgPicture('assets/images/recess.svg'),
+      buildSvgPicture('assets/images/knowledge.svg'),
     ]);
+
+    Widget button;
+    button = buildButton("Next", () {
+      counter++;
+      setState(() {
+        displayedPicture = pictures[counter];
+        displayedMessage = messages[counter];
+        displayedButton = buttons[counter];
+      });
+    });
+    buttons.add(button);
+    button = buildButton("Next", () {
+      counter++;
+      setState(() {
+        displayedPicture = pictures[counter];
+        displayedMessage = messages[counter];
+        displayedButton = buttons[counter];
+      });
+    });
+    buttons.add(button);
+    buttons.add(
+      buildButton("Get started", signin),
+    );
+
+    messages.addAll([
+      buildMessage(
+        "Learn anytime, and anywhere",
+        "Lorem ipsum dolor sit amet consectetur"
+            " adipiscing elit Ut et massa mi. Aliquam in hendrerit.",
+      ),
+      buildMessage(
+        "Unleash Your Creativity and Compete with Style",
+        "Lorem ipsum dolor sit amet consectetur"
+            " adipiscing elit Ut et massa mi. Aliquam in hendrerit.",
+      ),
+      buildMessage(
+          "High quality lectures and unlimited questions",
+          "Lorem ipsum dolor sit amet consectetur"
+              " adipiscing elit Ut et massa mi. Aliquam in hendrerit.")
+    ]);
+
+    displayedPicture = pictures[counter];
+    displayedMessage = messages[counter];
+    displayedButton = buttons[counter];
+  }
+
+  Widget buildMessage(String mainMessage, String subMessage) {
+    return SizedBox(
+      key: UniqueKey(),
+      height: 150,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              mainMessage,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+            const Spacer(),
+            Text(
+              subMessage,
+              overflow: TextOverflow.fade,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 13,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox buildButton(String label, VoidCallback onTap) {
+    return SizedBox(
+      key: UniqueKey(),
+      width: 200,
+      height: 50,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [Color(0xFF0396FF), Color.fromARGB(255, 133, 204, 255)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Center(
+            child: Text(
+              label,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSvgPicture(String assetName) {
+    return SizedBox(
+      key: UniqueKey(),
+      height: MediaQuery.of(context).size.height * 0.4,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SvgPicture.asset(assetName),
+      ),
+    );
+  }
+
+  Widget buildPageControlDots(int currentPageNumber) {
+    return Row(
+      key: UniqueKey(),
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        for (var index = 0; index < 3; index++)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: CircleAvatar(
+              radius: 4,
+              backgroundColor: index == currentPageNumber ? Colors.blue : Colors.blue[100],
+            ),
+          )
+      ],
+    );
   }
 
   Future signin() async {
-  
-  try {
+    try {
       final user = await GoogleSignInApi.login();
       String? name = user!.displayName;
       String? image = user.photoUrl;
 
       // SharedPreferences pref = await SharedPreferences.getInstance();
       UserPreferences.setuser(image!, name!);
-
-   } catch (error) {
-   // console.error("Error during login: ", error);
-      UserPreferences.setuser("https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50", "testDisplayName");
-  }
+    } catch (error) {
+      // console.error("Error during login: ", error);
+      UserPreferences.setuser(
+          "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+          "testDisplayName");
+    }
 
     Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => (MenuDashboardLayout())));
@@ -131,49 +194,29 @@ class _OnboardingState extends State<Onboarding> {
   Widget build(BuildContext context) {
     createWidgets();
     return CupertinoPageScaffold(
-      child: Stack(
+      child: Column(
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Color(0xFFABDCFF),
-                Color(0xFF0396FF),
-              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-            ),
+          const Spacer(),
+          AnimatedSwitcher(
+            duration: const Duration(seconds: 1),
+            child: displayedPicture,
           ),
-          Positioned(
-              top: 0, left: 0, child: Image.asset('assets/images/wave.png')),
-          Align(
-            alignment: Alignment.center,
-            child: PageView.builder(
-                controller: controller,
-                onPageChanged: (value) {
-                  setState(() {
-                    pageNumber = value;
-                  });
-                },
-                itemCount: 3,
-                itemBuilder: (context, index) => widgets[index]),
+          const Spacer(),
+          AnimatedSwitcher(
+            duration: const Duration(seconds: 1),
+            child: displayedMessage,
           ),
-          pageNumber == 2
-              ? Container()
-              : Positioned(
-                  bottom: 10,
-                  right: 10,
-                  child: CupertinoButton(
-                    child: Icon(
-                      pageNumber == 1
-                          ? BoxIcons.bx_check
-                          : BoxIcons.bx_chevron_right,
-                      color: Color(0xFFFFFFFF),
-                      size: 30,
-                    ),
-                    onPressed: () {
-                      controller.nextPage(
-                          duration: Duration(milliseconds: 300),
-                          curve: Curves.fastOutSlowIn);
-                    },
-                  ))
+          const Spacer(),
+          AnimatedSwitcher(
+            duration: const Duration(seconds: 1),
+            child: buildPageControlDots(counter),
+          ),
+          const Spacer(),
+          AnimatedSwitcher(
+            duration: const Duration(seconds: 1),
+            child: displayedButton,
+          ),
+          const Spacer(),
         ],
       ),
     );
