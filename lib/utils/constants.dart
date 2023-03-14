ec198d84c588b915018b47e6f3430eb0bb084d56
Esubalew <esubalew.a2009@gmail.com>
2023-03-14 10:04:50 +0300

    api base changed from "wp-json" to "api"

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@ class AppUrl {
   static var username = 'esubalew11@gmail.com';
   static var password = '105672864563817565';
   static var loginWith = 'google';
-  static var baseUrl = 'https://dashencon.com/test/wp-json/ds_lc/v1';
+  static var baseUrl = 'https://dashencon.com/test/api/ds_lc/v1';
   static var courseUrl = '$baseUrl/courses';
   static var lessonUrl = '$baseUrl/lesson';
 }
